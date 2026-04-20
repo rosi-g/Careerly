a1aaccff5666eb9a7c44ff4ce596aaa400387f79
rosi-g <rosessog@gmail.com>
2026-04-20 13:41:19 +0200

    Create page4.py

---
+++ pages/page4.py
@@ -0,0 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from job_page import show_job_page
+import streamlit as st
+
+career = st.session_state.get("selected_career", "Financial Analyst")
+show_job_page(career)
